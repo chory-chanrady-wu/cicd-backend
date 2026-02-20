cd208f27b1dcb076c10872a8c0e1f31c80613c02
chory-chanrady-wu <chorychanrady.wu@gmail.com>
2026-02-20 23:41:29 +0700

    updated SecurityConfig

--- src/main/java/com/chanrady/backend/config/SecurityConfig.java
+++ src/main/java/com/chanrady/backend/config/SecurityConfig.java
@@ -4,14 +4,36 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-            .formLogin(form -> form.disable());
+            .authorizeHttpRequests(auth -> auth.requestMatchers("/**").permitAll())
+            .formLogin(form -> form.disable())
+            .cors(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("https://cicd-frontend-woad.vercel.app/"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
